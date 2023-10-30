caf322ad1a5b1c67c4d71ecede0d1aab8be2297c
manikantagangisetty537@gmail.com <manikantagangisetty537@gmail.com>
2023-10-30 09:56:23 +0000

    Edited settings and bank_users.borrower_registration_main_form.borrower_profile

--- client_code/bank_users/borrower_registration_main_form/borrower_profile/__init__.py
+++ client_code/bank_users/borrower_registration_main_form/borrower_profile/__init__.py
@@ -14,3 +14,16 @@ class borrower_profile(borrower_profileTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    data=app_tables.borrower.search()
+    full_name=[]
+    email_id=[]
+    mobile_no=[]
+    date_of_birth=[]
+    city=[]
+    upload_pan_card=[]
+    full_name=self.label_2.text
+    email_id=self.label_4.text
+    mobile_no=self.label_6.text
+    date_of_birth=self.label_8.text
+    city=self.label_10.text
+    upload_pan_card=self.label_12.text
