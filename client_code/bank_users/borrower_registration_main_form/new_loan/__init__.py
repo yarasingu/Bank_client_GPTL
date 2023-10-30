5eb2bde7271d2cb8df0b8dca2aa5a1b5c415ced2
manikantagangisetty537@gmail.com <manikantagangisetty537@gmail.com>
2023-10-30 10:36:33 +0000

    Created bank_users.borrower_registration_main_form.new_loan; Edited settings, bank_users.borrower_registration_main_form.boorrower_edit_profile and bank_users.borrower_registration_main_form.borrower_profile

--- client_code/bank_users/borrower_registration_main_form/new_loan/__init__.py
+++ client_code/bank_users/borrower_registration_main_form/new_loan/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import borrower_profileTemplate
+from ._anvil_designer import new_loanTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,18 +8,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class borrower_profile(borrower_profileTemplate):
+class new_loan(new_loanTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    data=app_tables.borrower.search()
-   
-    full_name=self.label_2.text
-    email_id=self.label_4.text
-    mobile_no=self.label_6.text
-    date_of_birth=self.label_8.text
-    
-    city=self.label_10.text
-    upload_pan_card=self.label_12.text
