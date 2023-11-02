5cb5bfe0ec0653ccf23ba2a2fba4a98d937ad223
yarisingutarun@gmail.com <yarisingutarun@gmail.com>
2023-11-02 06:12:19 +0000

    Created borrower_registration_form.star_1_borrower_registration_form_begin; Edited bank_users.user_form; Deleted borrower_registration_form.star_1_borrower_registration_form and borrower_registration_form.star_1_borrower_registration_form_step_2

--- client_code/borrower_registration_form/star_1_borrower_registration_form_begin/__init__.py
+++ client_code/borrower_registration_form/star_1_borrower_registration_form_begin/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import star_1_borrower_registration_formTemplate
+from ._anvil_designer import star_1_borrower_registration_form_beginTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,14 +8,15 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class star_1_borrower_registration_form(star_1_borrower_registration_formTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
+class star_1_borrower_registration_form_begin(star_1_borrower_registration_form_beginTemplate):
+  def __init__(self, user_id,**properties):
+    self.userId = user_id
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
 
   def next_butto_for_step_2_click(self, **event_args):
+    
     open_form('borrower_registration_form.star_1_borrower_registration_form1')
 
   def button_1_click(self, **event_args):
