85ffad7452f4ec5f96ccd6636e20e812e2502b5d
sandhyasiddu12@gmail.com <sandhyasiddu12@gmail.com>
2023-11-02 06:56:40 +0000

    Edited bank_users.borrower_registration_main_form.new_loan_request.loan_type

--- client_code/bank_users/borrower_registration_main_form/new_loan_request/loan_type/__init__.py
+++ client_code/bank_users/borrower_registration_main_form/new_loan_request/loan_type/__init__.py
@@ -31,3 +31,12 @@ class loan_type(loan_typeTemplate):
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     alert('submitted')
+
+  def drop_down_1_change(self, **event_args):
+
+    value=self.drop_down_1.selected_value 
+    if value=="K-12 Educational loan":
+      open_form('bank_users.borrower_registration_main_form.new_loan_request.k12_loan')
+    else:
+      open_form('bank_users.borrower_registration_main_form.new_loan_request.business_loan')
+  
