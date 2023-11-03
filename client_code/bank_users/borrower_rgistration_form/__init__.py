404ebe57e459d95a680785d8a90794211bdc1f57
manikantagangisetty537@gmail.com <manikantagangisetty537@gmail.com>
2023-11-03 07:42:17 +0000

    Created bank_users.borrower_rgistration_form, bank_users.borrower_rgistration_form.application_tracker, bank_users.borrower_rgistration_form.application_tracker.RowTemplate1, bank_users.borrower_rgistration_form.boorrower_edit_profile and 5 more; Deleted bank_users.borrower_registration_form, bank_users.borrower_registration_form.application_tracker, bank_users.borrower_registration_form.application_tracker.RowTemplate1, bank_users.borrower_registration_form.boorrower_edit_profile and 5 more

--- client_code/bank_users/borrower_rgistration_form/__init__.py
+++ client_code/bank_users/borrower_rgistration_form/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import borrower_registration_formTemplate
+from ._anvil_designer import borrower_rgistration_formTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,7 +8,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class borrower_registration_form(borrower_registration_formTemplate):
+class borrower_rgistration_form(borrower_rgistration_formTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
