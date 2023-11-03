a11d1a14f265e58639d91f94d7f89a1e9b0b1cc3
yarisingutarun@gmail.com <yarisingutarun@gmail.com>
2023-11-03 10:55:20 +0000

    Edited borrower_server, settings and bank_users.borrower_rgistration_form

--- server_code/borrower_server.py
+++ server_code/borrower_server.py
@@ -15,6 +15,7 @@ def add_borrower_step1(full_name,mobile_no,dob,user_id):
     row[0]['full_name'] = full_name
     row[0]['mobile'] = mobile_no
     row[0]['date_of_birth'] = dob
+    
 
 @anvil.server.callable
 def add_borrower_step2(gender,user_photo,city,user_id):
@@ -23,6 +24,8 @@ def add_borrower_step2(gender,user_photo,city,user_id):
     row[0]['gender']=gender
     row[0]['user_photo']=user_photo
     row[0]['city']=city
+
+
 @anvil.server.callable
 def add_borrower_step3(aadhar,aadhar_card,pan,pan_card,user_id):
   row=app_tables.user_profile.search(coustmer_id=user_id)
