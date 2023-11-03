ba331653ae4146f9581d31c020efd4e8a33a4f3d
manikantagangisetty537@gmail.com <manikantagangisetty537@gmail.com>
2023-11-03 04:58:10 +0000

    Edited borrower_registration_form.star_1_borrower_registration_form_begin_3

--- client_code/borrower_registration_form/star_1_borrower_registration_form_begin_3/__init__.py
+++ client_code/borrower_registration_form/star_1_borrower_registration_form_begin_3/__init__.py
@@ -14,3 +14,15 @@ class star_1_borrower_registration_form_begin_3(star_1_borrower_registration_for
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    aadhar= sel
+    user_photo = self.borrower_registration_img_file_loader.file
+    city = self.borrower_registration_cit_text.text
+    user_id = self.userId
+    if not gender or not user_photo or not city:
+      Notification("Please Fill The All required fileds")
+    else:
+      anvil.server.call('add_borrower_step2',gender,user_photo,city,user_id)
+
