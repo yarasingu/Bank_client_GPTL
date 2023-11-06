a8078fcf95dc92bf1e2282e8f070a817c1884248
yarisingutarun@gmail.com <yarisingutarun@gmail.com>
2023-11-06 05:59:47 +0000

    Edited borrower_registration_form.star_1_borrower_registration_form_begin_4

--- client_code/borrower_registration_form/star_1_borrower_registration_form_begin_4/__init__.py
+++ client_code/borrower_registration_form/star_1_borrower_registration_form_begin_4/__init__.py
@@ -18,3 +18,14 @@ class star_1_borrower_registration_form_begin_4(star_1_borrower_registration_for
 
   def home_borrower_registration_form_copy_1_click(self, **event_args):
     open_form('bank_users.user_form')
+
+  def submit_4_borrower_registration_form_click(self, **event_args):
+    mother_toung = self.borrower_registration_mother_tounge_text.text
+    marital_status = self.marital_status_borrower_registration_dropdown.selected_value
+    spouse_name = self.borrower_registration_spouse_name_text.text
+    marrege_date = self.marriage_date_date_pickeer.date
+    user_id = self.userId
+    if not marital_status or not mother_toung or not spouse_name or not marrege_date
+    
+    
+    
